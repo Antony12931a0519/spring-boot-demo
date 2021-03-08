e48a4c6fed378863feb3674470d7177464a5c723
Antony Sampath kumar Reddy Avula <antonysampath333@gmail.com>
2021-03-08 23:18:45 +0530

    Add files via upload
    initial cut

---
+++ springboot-demo-app/src/com/demo/springbootdemoapp/SpringbootDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdemoapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoAppApplication.class, args);
+	}
+
+}
---
+++ springboot-demo-app/src/test/java/com/demo/springbootdemoapp/SpringbootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdemoapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
